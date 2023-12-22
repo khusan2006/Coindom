ee8c41b576bc13611ad3eebc207538be6adebfc9
khusan2006 <bahodirovkhusan@gmail.com>
2023-12-22 14:54:52 +0500

    created the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home'
+import InfoPage from './pages/InfoPage/InfoPage';
+import Navbar from './components/navbar/Navbar';
 
-function App() {
+const App = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolled = window.scrollY > 100; // Change '100' to the scroll position where you want the navbar to become fixed
+      setIsScrolled(scrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+
+      <BrowserRouter>
+      <Navbar isScrolled={isScrolled} />
+        <Routes>
+          <Route path='/' element={<Home isScrolled={isScrolled} />} />
+          <Route path='/:name' element={<InfoPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
