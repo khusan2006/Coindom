7619ea23d6de1dfea218cb101a7310f62239bbef
khusan2006 <bahodirovkhusan@gmail.com>
2023-12-23 16:19:46 +0500

    make navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { IconBrandDiscord, IconBrandTwitter } from "@tabler/icons-react";
+import React, { useState } from "react";
+import { IconBrandDiscord, IconBrandTwitter, IconMenu2, IconX } from "@tabler/icons-react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 const Navbar = ({isScrolled}) => {
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <header className={isScrolled ? 'fixed': ''}>
@@ -25,8 +26,27 @@ const Navbar = ({isScrolled}) => {
         <div className="icons">
           <IconBrandDiscord className="icon" />
           <IconBrandTwitter className="icon" />
+          <IconMenu2 className="hidden" onClick={() => setIsOpen(true)} />
         </div>
       </nav>
+      
+         <ul className={`mobile-menu ${isOpen && 'open'}`}>
+          <IconX className="close-btn" onClick={() => setIsOpen(false)}/>
+         <li className="mobile-link">
+           <a href="#home">Home</a>
+         </li>
+         <li className="mobile-link">
+           <a href="#market">Market</a>
+         </li>
+         <li className="mobile-link">
+           <a href="#choose">Choose Us</a>
+         </li>
+         <li className="mobile-link">
+           <a href="#join">Join</a>
+         </li>
+       </ul>
+      
+
     </header>
   );
 };
